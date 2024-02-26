22af26ec715e86a1d7969341450b575000368841
Arnab <grinit10@gmail.com>
2024-02-27 01:03:29 +1100

    Initial commit.

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export const DashBoardPage = () => {
+    return (
+        <div>
+            <h1>Hello from Dashboard(Protected)</h1>
+        </div>
+    )
+}
+
+export default DashBoardPage
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export const LandingPage = () => {
+  return (
+      <div>
+          <h1>Hello from Landing page(Open)</h1>
+      </div>
+  )
+}
+
+export default LandingPage
